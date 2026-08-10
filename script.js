b2620f877c69cb04877e54e0d2af3a3c711b1ddf
Nexorithm <mayankmadhukar67@gmail.com>
2026-08-10 15:01:57 +0530

    Set up JavaScript variables for all 9 grid positions
    Added player element and  Styled each grid item with flexbox for center alignment.

---
+++ script.js
@@ -0,0 +1,69 @@
+let player = document.createElement("div");
+player.innerText = "Player-1"
+
+let iswin = false;
+
+let div1 = document.querySelector(".grid-item-1");
+let div2 = document.querySelector(".grid-item-2");
+let div3 = document.querySelector(".grid-item-3");
+let div4 = document.querySelector(".grid-item-4");
+let div5 = document.querySelector(".grid-item-5");
+let div6 = document.querySelector(".grid-item-6");
+let div7 = document.querySelector(".grid-item-7");
+let div8 = document.querySelector(".grid-item-8");
+let div9 = document.querySelector(".grid-item-9");
+
+// Styling grid items
+let inneritems = document.querySelector(".grid-item-1");
+inneritems.style.display = "flex";
+inneritems.style.flexDirection = "row";
+inneritems.style.justifyContent = "center";
+inneritems.style.alignItems = "center";
+
+let inneritems2 = document.querySelector(".grid-item-2");
+inneritems2.style.display = "flex";
+inneritems2.style.flexDirection = "row";
+inneritems2.style.justifyContent = "center";
+inneritems2.style.alignItems = "center";
+
+let inneritems3 = document.querySelector(".grid-item-3");
+inneritems3.style.display = "flex";
+inneritems3.style.flexDirection = "row";
+inneritems3.style.justifyContent = "center";
+inneritems3.style.alignItems = "center";
+
+let inneritems4 = document.querySelector(".grid-item-4");
+inneritems4.style.display = "flex";
+inneritems4.style.flexDirection = "row";
+inneritems4.style.justifyContent = "center";
+inneritems4.style.alignItems = "center";
+
+let inneritems5 = document.querySelector(".grid-item-5");
+inneritems5.style.display = "flex";
+inneritems5.style.flexDirection = "row";
+inneritems5.style.justifyContent = "center";
+inneritems5.style.alignItems = "center";
+
+let inneritems6 = document.querySelector(".grid-item-6");
+inneritems6.style.display = "flex";
+inneritems6.style.flexDirection = "row";
+inneritems6.style.justifyContent = "center";
+inneritems6.style.alignItems = "center";
+
+let inneritems7 = document.querySelector(".grid-item-7");
+inneritems7.style.display = "flex";
+inneritems7.style.flexDirection = "row";
+inneritems7.style.justifyContent = "center";
+inneritems7.style.alignItems = "center";
+
+let inneritems8 = document.querySelector(".grid-item-8");
+inneritems8.style.display = "flex";
+inneritems8.style.flexDirection = "row";
+inneritems8.style.justifyContent = "center";
+inneritems8.style.alignItems = "center";
+
+let inneritems9 = document.querySelector(".grid-item-9");
+inneritems9.style.display = "flex";
+inneritems9.style.flexDirection = "row";
+inneritems9.style.justifyContent = "center";
+inneritems9.style.alignItems = "center";
